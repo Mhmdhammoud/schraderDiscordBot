29bda714d3048574864f75b97c4ce331cf2b9e88
sstorrmm <dr.co0obra@hotmail.com>
2020-07-24 18:40:33 +0300

    Updated readme

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 ### Schrader Beer Discord Bot v1.0.0
 
-## Basic commands
+## Basic commands-TODOS
 
 - [x] Get server stats
-- []
+- Check simley face permission first
+- [x] Prune messages
+- Check smilely face permission first
+- [ ] Music
+- Any one can use music
+- [ ] Save media to DB
+- [ ] TTS
+- STTS (not dota exclusive)
+- [ ] Help
+- [ ] Games
+- Trivia
+- Fight
+- Tic Tac Toe
+- [ ] Fanstasy games
+- [ ] User info
--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
+/* eslint-disable linebreak-style */
 // require the discord.js module
+
 const Discord = require('discord.js');
+
 const { prefix, token } = require('./config.json');
+
 // create a new Discord client
+
 const client = new Discord.Client();
+
 const firstBatchCommands = require('./command/test');
+
 client.commands = new Discord.Collection();
+
 client.commands.set(firstBatchCommands.name, firstBatchCommands);
 
 // when the client is ready, run this code
+
 // this event will only trigger one time after logging in
+
 client.once('ready', () => {
 	console.log('Boop beep, beep boop!');
 });
 
 // login to Discord witprh your app's toke\n
+
 client.login(token);
+
 const getUserFromMention = (mention) => {
 	if (!mention) return;
 
@@ -31,36 +43,49 @@ const getUserFromMention = (mention) => {
 
 client.on('message', (message) => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
 	if (message.content.startsWith(`${prefix}server`)) {
 		return message.channel
+
 			.send(
 				`Server name: ${message.guild.name}
+
 			\n Total number of members: ${message.guild.memberCount}
+
 			\n Server region: ${message.guild.region} 
+
 			\n Created at: ${message.guild.createdAt}`
 			)
+
 			.catch((err) => {
 				console.error(err);
 			});
 	} else if (message.content === `${prefix}`) {
 		message
+
 			.reply(
 				`You need to add an argument after your prefix. For example: ${prefix}server `
 			)
+
 			.catch((err) => console.error(err));
 	}
-	// split the args from the prefix
-	const args = message.content.slice(prefix.length).trim().split(' ');
-	// Move the commands alone with lowercase transformation
-	const command = args.shift().toLowerCase();
 
+	// split the args from the prefix
+
+	const args = message.content.slice(prefix.length).trim().split(' ');
+
+	// Move the commands alone with lowercase transformation
+
+	const command = args.shift().toLowerCase();
+	let user;
 	if (!args.length) {
 		return message.reply(`You didn't provide any arguments, ${message.author}!`);
 	} else if (command === 'warn') {
 		if (!message.mentions.users.size) {
 			return message.reply('you need to tag a user in order to warn them!');
 		} else {
-			const user = getUserFromMention(args[0]);
+			user = getUserFromMention(args[0]);
+
 			if (!user) {
 				message.reply('Please choose a valid user in order to warn him');
 			} else {
@@ -71,15 +96,29 @@ client.on('message', (message) => {
 		}
 	} else if (command === 'prune') {
 		const amount = parseInt(args[0]) + 1;
+
 		if (isNaN(amount)) {
 			message.channel.bulkDelete(1);
+
 			return message.reply(`${args[0]}that doesnt seem to be a valid number.`);
 		} else if (amount <= 1 || amount > 100) {
 			return message.reply('Choose a number between 1 and 99!');
 		} else {
 			message.channel
+
 				.bulkDelete(amount, true)
+
 				.catch((err) => console.error(err.message));
+		}
+	} else if (command === 'user-info') {
+		user = getUserFromMention(args[0]);
+		console.log(args[0]);
+
+		if (!user) {
+			return message.channel.send('Please tag someone to check his user info');
+		} else {
+			message.reply(`User Created At ${message.mentions.users[0]}`);
+			console.log(user.id);
 		}
 	}
 });
